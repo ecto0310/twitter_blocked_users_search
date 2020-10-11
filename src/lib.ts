6211dc5a4ab5cac091727c30338d57244d67582e
ecto0310 <ecto0310@gmail.com>
2020-10-11 18:21:35 +0900

    Add function to verify certificate

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,21 @@
 import { config } from "dotenv";
 import Twitter from "twitter";
 
+type Task =
+  // Verify account validity
+  | {
+    type: "authUserId"
+  };
+
+type State = {
+  // Authenticated User
+  authUserId: string;
+  // Task queue
+  tasks: Task[];
+};
+
 let client: Twitter;
+let state: State; 
 
 // Initialization phase
 export function init() {
@@ -12,4 +26,33 @@ export function init() {
     access_token_key: process.env["access_token"]!,
     access_token_secret: process.env["access_token_secret"]!
   });
+}
+
+// Main process
+export async function main() {
+  while (state.tasks.length !== 0) {
+    await progress();
+  }
+  console.log("Succeed all process.");
+}
+
+// Process
+async function progress() {
+  const task = state.tasks.shift();
+  if (task === undefined) {
+    throw new Error("Tasks is empty");
+  }
+  switch (task.type) {
+    // Verify account validity
+    case "authUserId": {
+      await authUserId();
+      return;
+    }
+  }
+}
+
+// Verify account validity
+async function authUserId() {
+  const res = await client.get("account/verify_credentials", {});
+  state.authUserId = res.id_str;
 }
