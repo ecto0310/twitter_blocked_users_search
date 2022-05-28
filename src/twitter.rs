a08f4a7bba5ecd51c876d039d1e4ea60ecadbc09
ecto0310 <ecto0310@gmail.com>
2022-05-29 01:01:35 +0900

    [add] authorization header

--- src/twitter.rs
+++ src/twitter.rs
@@ -67,6 +67,42 @@ impl Twitter {
         endpoint: &str,
         parameters: Vec<(&str, &str)>,
     ) -> String {
+        let nonce = format!("{}", chrono::Utc::now().timestamp());
+        let timestamp = format!("{}", chrono::Utc::now().timestamp());
+        let mut parameters = parameters;
+        parameters.push(("oauth_consumer_key", &self.consumer_key));
+        parameters.push(("oauth_nonce", &nonce));
+        parameters.push(("oauth_signature_method", "HMAC-SHA1"));
+        parameters.push(("oauth_timestamp", &timestamp));
+        parameters.push(("oauth_token", &self.access_token));
+        parameters.push(("oauth_version", "1.0"));
+
+        let oauth_signature = self.get_oauth_signature(method, endpoint, parameters.clone());
+        parameters.push(("oauth_signature", &oauth_signature));
+        parameters.sort();
+
+        format!(
+            "OAuth {}",
+            parameters
+                .into_iter()
+                .map(|(key, value)| {
+                    format!(
+                        r#"{}="{}""#,
+                        percent_encoding::utf8_percent_encode(key, &Self::FRAGMENT),
+                        percent_encoding::utf8_percent_encode(value, &Self::FRAGMENT)
+                    )
+                })
+                .collect::<Vec<String>>()
+                .join(", ")
+        )
+    }
+
+    fn get_oauth_signature(
+        &self,
+        method: &str,
+        endpoint: &str,
+        parameters: Vec<(&str, &str)>,
+    ) -> String {
         "".to_string()
     }
 }
